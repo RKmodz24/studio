261db7fff1f1914faf1e41a75208729c3ab8c2fe
Bindass_ Rk_MUSIC_and_status (Mr.Rk) <riteshkrmahato28@gmail.com>
2025-09-24 17:44:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8679056346-13abe",
+  "appId": "1:259266448690:web:e778bdc13df3a9254c5b2b",
+  "apiKey": "AIzaSyC9hSEHxbQq3Sy3rLyHeldk1CLePVFajk4",
+  "authDomain": "studio-8679056346-13abe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "259266448690"
+};
